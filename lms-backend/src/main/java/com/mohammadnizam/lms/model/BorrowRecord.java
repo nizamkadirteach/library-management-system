6689f2428ec8ddbcd2c0da3e588646573ae9b555
Learnio <nizamkadirteach@gmail.com>
2025-07-18 11:01:15 +0800

    Add BorrowRecord entity

---
+++ lms-backend/src/main/java/com/mohammadnizam/lms/model/BorrowRecord.java
@@ -0,0 +1,38 @@
+package com.mohammadnizam.lms.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "borrow_records")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_id")
+    private Integer recordId;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @Column(name = "borrow_date")
+    private LocalDate borrowDate;
+
+    @Column(name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    private BigDecimal fine;
+}
