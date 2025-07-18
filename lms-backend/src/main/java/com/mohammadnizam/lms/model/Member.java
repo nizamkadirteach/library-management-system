0ea7754a67e79da132c287a7cd5f26248c22d992
Learnio <nizamkadirteach@gmail.com>
2025-07-18 10:56:31 +0800

    Add Member JPA entity

---
+++ lms-backend/src/main/java/com/mohammadnizam/lms/model/Member.java
@@ -0,0 +1,36 @@
+package com.mohammadnizam.lms.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "members")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Integer memberId;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "contact_info")
+    private String contactInfo;
+
+    private String address;
+
+    @Column(name = "membership_start")
+    private LocalDate membershipStart;
+
+    @Column(name = "membership_end")
+    private LocalDate membershipEnd;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
