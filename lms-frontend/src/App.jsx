471b846a23065227084f6b651b51eba27feaead2
Learnio <nizamkadirteach@gmail.com>
2025-07-18 16:48:55 +0800

    Format HomePage with Prettier

--- lms-frontend/src/App.jsx
+++ lms-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
 import BookListPage from './pages/BookListPage'
 import MemberListPage from './pages/MemberListPage'
 import BorrowRecordPage from './pages/BorrowRecordPage'
@@ -11,20 +12,19 @@ export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
-          path="/"
           element={
             <ProtectedRoute>
               <SidebarLayout />
             </ProtectedRoute>
           }
         >
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="books" element={<BookListPage />} />
-          <Route path="members" element={<MemberListPage />} />
-          <Route path="borrow-records" element={<BorrowRecordPage />} />
-          <Route index element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/books" element={<BookListPage />} />
+          <Route path="/members" element={<MemberListPage />} />
+          <Route path="/borrow-records" element={<BorrowRecordPage />} />
         </Route>
       </Routes>
     </Router>
